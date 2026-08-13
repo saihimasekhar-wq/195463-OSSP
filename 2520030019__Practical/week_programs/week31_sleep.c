eda6f9c227c066147a923cf57b74745a7bad1cba
vipparthinavadeep <navadeepvipparthi@klh.edu.in>
2026-08-13 03:56:01 +0000

    add week31

--- 2520030019__Practical/week_programs/week31_sleep.c
+++ 2520030019__Practical/week_programs/week31_sleep.c
@@ -1,30 +1,42 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<stdlib.h>
-int main(){
-	int pid;
-	printf("start\n");
-	pid = fork(); //create child process
-	if(pid == 0){ // body of child process
-		printf("start of child\n");
-		sleep(15); // child process sleep for 15sec
-		exit(0); // force termination of child without completing child process
-		printf("end of child\n");
-	}
-	else { // body of parent process
-		sleep(10); // parent process sleep for 5 sec
-		wait(NULL); // parent process wait for child process to complete
-		printf("end\n");
-	}
-		return 0;
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main() {
+    pid_t pid;
+
+    printf("Start\n");
+
+    pid = fork();   // Create a child process
+
+    if (pid == 0) {   // Child process
+        printf("Start of child\n");
+
+        sleep(15);    // Child sleeps for 15 seconds
+
+        printf("End of child\n");
+
+        exit(0);      // Child process terminates
+    }
+    else if (pid > 0) {   // Parent process
+        sleep(10);        // Parent sleeps for 10 seconds
+
+        wait(NULL);       // Wait for child process to finish
+
+        printf("End\n");
+    }
+    else {
+        printf("Fork failed.\n");
+    }
+
+    return 0;
 }
-/* 
+/*
 OUTPUT:
-
-start
-start of child
+Start
+Start of child
 End of child
-end 
+End
 */
-
